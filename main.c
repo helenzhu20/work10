d1b1e06b63966b23c05be376d4e84de464bc2ed3
Helen Zhu <hzhu20@stuy.edu>
2021-10-19 08:25:24 -0400

    added more test statements

--- main.c
+++ main.c
@@ -9,7 +9,12 @@ int main() {
     struct pokemon *pidgey = insert_front(eevee, "pidgey", pokeggroups[5]);
     struct pokemon *oddish = insert_front(pidgey, "oddish", pokeggroups[1]); 
 
+    printf("list:\n");
     print_list(oddish);
+
+    printf("\nremove something not in list:\n");
+    struct pokemon *r = remove_pokemon(oddish, "pikachu");
+    print_list(r);
 
     printf("\nremove pidgey:\n");
     struct pokemon *rp = remove_pokemon(oddish, "pidgey");
@@ -18,6 +23,10 @@ int main() {
     printf("\nremove eevee:\n");
     struct pokemon *re = remove_pokemon(oddish, "eevee");
     print_list(re);
+
+    printf("\nremove oddish:\n");
+    struct pokemon *ro = remove_pokemon(oddish, "oddish");
+    print_list(ro);
     
     free_list(oddish);
     
